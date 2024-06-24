eab6af4f4749ac87cbea3b237e9e26750d3e5ad6
DHIADACH <dhiadachraoui41@gmail.com>
2024-06-24 12:06:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let myname = "ibtihel"
+myname ="helllo"
+console.log(myname)
+const num= 52.5
+console.log(num * 6)
+console.log(7==9)
+let x = null
+console.log(x)
+const names = ["youssef", "esra", "asma", "nour"]
+
+const person = {
+    firstName:"yasmine",
+    lastName: "Tebourski",
+    school: 1
+}
+console.log(person)
+console.log(person.lastName)
+for (let i = 0; i<
+     names.length; i++){
+    console.log(names[i])
+    console.log(names[i].length)
+}
+const students = [
+    {
+        firstName:"yasmine",
+        lastName: "Tebourski",
+        school: 1
+    },{
+        firstName:"dhia",
+        lastName: "dachraoui",
+        school: 5
+    },{
+        firstName:"khalil",
+        lastName: "ben kram",
+        school: 4
+    }
+]
+console.log(students[1].firstName)
